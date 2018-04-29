910709c8b08c18b7e3908ecaa17341f86dc397e3
Wesley Hanson <wrh1600@gmail.com>
2018-04-28 21:18:33 -0700

    done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require("body-parser");
-var PORT = process.env.PORT || 8080;
 var app = express();
+var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
